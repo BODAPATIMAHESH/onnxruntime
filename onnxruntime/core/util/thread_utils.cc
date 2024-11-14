3ce4bd809b774986cd0de609765f6e15f8c36419
Mahesh Bodapati <bmahi496@linux.ibm.com>
2024-11-14 05:34:34 -0500

    update threadpool setup to physical cores: it's giving better with MMA fix.

--- onnxruntime/core/util/thread_utils.cc
+++ onnxruntime/core/util/thread_utils.cc
@@ -114,14 +114,7 @@ CreateThreadPoolHelper(Env* env, OrtThreadPoolParams options) {
         return nullptr;
       }
 
- #if defined(PPC64_CPUINFO_SUPPORTED)
-      if (Env::Default().GetSmtmode() == 8)
-	      options.thread_pool_size = (static_cast<int>(default_affinities.size())) * 4;
-      else
-	      options.thread_pool_size = (static_cast<int>(default_affinities.size())) * (Env::Default().GetSmtmode());
-#else
       options.thread_pool_size = static_cast<int>(default_affinities.size());
-#endif
       to.affinities = std::move(default_affinities);
 #endif
     } else {
